570da483c46f100982a66c54566d5d25c8268bb9
Santosh jogdand <santoshjogdand256@gmail.com>
2025-02-12 22:44:04 +0530

    add database connectivity

---
+++ backend/DB/database.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB= async()=>{
+    try{
+        const connectionInstance  = await mongoose.connect(`${process.env.MONGO_URI}`)
+        console.log("DB Connection Successful",connectionInstance.connection.host)
+
+    }catch(err){
+        console.log("ERR in db connection: ",err)      
+    }
+}
+
+export default connectDB;

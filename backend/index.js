f839718d8885d4ac4292851bc765eec853d333b7
Santosh jogdand <santoshjogdand256@gmail.com>
2025-02-12 22:29:15 +0530

    feat: initialize server with database connection and environment config

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,15 @@
-import express from "express"
-const app = express()
+import {app} from "./app.js"
+import connectDB from "./DB/database.js"
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB().then(()=>{
+    app.listen(8080,()=>{
+        console.log("App listening on port 8080")
+    })
+}).catch((err)=>{
+    console.log("Error while connecting DB",err)
+})

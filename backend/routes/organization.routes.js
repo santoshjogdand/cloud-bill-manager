dd5bb99f1c93f19751f3a60fe36f5ab6456d2043
Santosh jogdand <santoshjogdand256@gmail.com>
2025-02-21 14:06:39 +0530

    Add inventory routes for product and UOM management

--- backend/routes/organization.routes.js
+++ backend/routes/organization.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {register,login,logout} from "../controllers/organization.controller.js";
 import {allCustomers, createCustomer,getCustomer} from "../controllers/customer.controller.js";
+import { addUOM,addProduct,getUOMS, getProducts } from "../controllers/inventory.controller.js";
 import verifyJwt from "../middlewares/auth.middleware.js";
 
 const router = Router()
@@ -10,7 +11,14 @@ router.route("/login").post(login)
 router.route("/logout").post(logout)
 
 //secure routes
+//Customer Routes
 router.route("/createCustomer").post(verifyJwt,createCustomer)
 router.route("/Customers").post(verifyJwt,getCustomer).get(verifyJwt,allCustomers)
+
+//Inventory routes
+router.route("/addProduct").post(verifyJwt,addProduct)
+router.route("/getProducts").get(verifyJwt,getProducts)
+router.route("/addUOM").post(verifyJwt,addUOM)
+router.route("/getUOMS").get(verifyJwt,getUOMS)
 
 export default router

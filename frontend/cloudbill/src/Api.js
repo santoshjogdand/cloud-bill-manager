d6bea48fb00ae48b0b61803e3c95b4f0a6081528
Santosh jogdand <santoshjogdand256@gmail.com>
2025-05-19 08:22:17 +0530

    switched backend hosting to vercel

--- frontend/cloudbill/src/Api.js
+++ frontend/cloudbill/src/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://cloud-bill-manager.onrender.com/api/v1/organization/",
+    baseURL: "https://cloud-bill-manager.vercel.app/api/v1/organization/",
     withCredentials: true, 
     headers: {
         'Content-Type': 'application/json'
